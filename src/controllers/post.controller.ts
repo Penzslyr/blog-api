51969171cadd0344648bb923b0142565586d5437
Effendi Prakoso <Fendi.prakoso048@gmail.com>
2025-02-25 03:18:43 +0700

    return user data on tweets

--- src/controllers/post.controller.ts
+++ src/controllers/post.controller.ts
@@ -126,9 +126,7 @@ export const getAllPosts = async (
   res: Response
 ): Promise<void> => {
   try {
-    const posts = await Post.find()
-      .populate("user", "email")
-      .sort({ createdAt: -1 });
+    const posts = await Post.find().populate("user").sort({ createdAt: -1 });
     res.status(200).json(posts);
   } catch (error) {
     res.status(500).json({ message: "Server error" });
